355f2d43f3d6dfadf674c98654009a1f865afe7e
martin-biolcati <martinbiolcati@gmail.com>
2022-11-18 09:56:48 -0300

    detalles imagenes

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -15,12 +15,14 @@ fetch(url)
         console.log(data);
 
         let section = document.querySelector('.detalle')
-        let url_img = 'https://image.tmdb.org/t/p/w400/'
+        let url_img = 'https://image.tmdb.org/t/p/w300/'
         section.innerHTML += `<article>
                                 <img src="${url_img + data.poster_path}">
                                 <h2>Nombre: ${data.original_title}</h2>
-                                <h3>Clasificacion: ${data.vote_average}</h3>
-                                <h3>Sinopsis: ${data.overview}</h3>
+                                <h3>Clasificacion:</h3> <a>${data.vote_average}</a>
+                                <h3>Sinopsis:</h3> <a> ${data.overview}</a>
+                                <h3>Genero:</h3><a> ${data.genres[0].name}</a>
+                                <h3>Duracion: </h3><a>${data.runtime} minutos</a>
                             </article>`   
     })
     .catch(function(error){
--- js/favorite.js
+++ js/favorite.js
@@ -16,6 +16,7 @@ if (elegidos == null || elegidos.length == 0){
 }
 function buscarYMostrarFavoritos (id){
     let url = `https://api.themoviedb.org/3/movie/${id}?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US`
+    let url_img = 'https://image.tmdb.org/t/p/w200/'
 
     fetch (url)
         .then(function(response){
@@ -24,7 +25,7 @@ function buscarYMostrarFavoritos (id){
         .then(function(data){
             console.log(data);
             lista_fav.innerHTML += `<article>
-                                <img src="${data.poster_path}">
+                                <img src="${url_img + data.poster_path}" >
                                 <h2>Nombre: ${data.original_title}</h2>
                             </article>`
         })
