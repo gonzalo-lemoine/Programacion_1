ae8073738e0e5b0a60b3de8b598623fe4506290b
BautistaGiorgi <jgiorgi@udesa.edu.ar>
2022-11-19 14:47:37 -0300

    detailgenres

--- js/detail_genres.js
+++ js/detail_genres.js
@@ -1,3 +1,24 @@
 let queryString = location.search; 
 let queryStringToObject = new URLSearchParams(queryString);
 let id = queryStringToObject.get('id'); 
+let url_series = `https://api.themoviedb.org/3/discover/tv?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=${id}&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
+
+console.log(url_series)
+
+fetch(url_series)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        let section = document.querySelector('.generos_movie')
+        let url_img = 'https://image.tmdb.org/t/p/w300/'
+        section.innerHTML += `<div class="detalle">
+                                <h2>${data.genre_ids[1]}</h2>
+                            </div>`   
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+    
--- js/genres.js
+++ js/genres.js
@@ -6,34 +6,13 @@ fetch(url_generos)
     .then(function(data){
         console.log(data)
         let info = data.genres
-        let conteiner= document.querySelector('.menu');
-        let titulos_generos ='';
-        for (let i=0; i<info.length; i++){
-            titulos_generos += `<article>
-                                    <a href="#go${info[i].name}">${info[i].name}</a> |
-                                </article>`
-
-        }
-        conteiner.innerHTML = titulos_generos;
-    })
-    .catch(function(error){
-        console.log(error);
-    })
-
-
-fetch(url_generos)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        let info = data.genres
         let conteiner= document.querySelector('.allgenres');
         let titulos_generos ='';
         for (let i=0; i<info.length; i++){
             titulos_generos += `<article class="${info[i].name}">
-                                    <A name="go${info[i].name}"></A>
-                                    <h1>${info[i].name}</h1>
+                                    <A href='detail-genres.html?id=${info[i].id}'>
+                                        <h2>${info[i].name}</h2>
+                                    </A>
                                 </article>`
 
         }
@@ -42,14 +21,3 @@ fetch(url_generos)
     .catch(function(error){
         console.log(error);
     })
-url_movies= "https://api.themoviedb.org/3/discover/tv?api_key=0317bbf7efac7dd04b2c2c3748377d57"
-fetch(url_movies)
-.then(function(response){
-    return response.json();
-})
-.then(function(data){
-    console.log(data)
-})
-.catch(function(error){
-    console.log(error);
-})
