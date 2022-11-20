d6821905e19ef05b70e7cbd100e296c1a83ab310
BautistaGiorgi <jgiorgi@udesa.edu.ar>
2022-11-20 18:36:39 -0300

    Carrusel en todas las secciones

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ fetch (url_pelis_pop)
         console.log(data)
         let info = data.results
         let conteiner = document.querySelector('.carrusel');
-        let url_img = 'https://image.tmdb.org/t/p/w100/'
         let peliculas_populares = '';
 
         for(let i = 0; i<info.length; i++){
@@ -26,8 +25,6 @@ fetch (url_pelis_pop)
     })
 
     let fila = document.querySelector('.contenedor_carrusel');
-    let peliculas = document.querySelectorAll('.pelis');
-    
     let flechaIzquierda = document.getElementById('flecha_izquierda');
     let flechaDerecha = document.getElementById('flecha_derecha');
     
@@ -41,7 +38,7 @@ fetch (url_pelis_pop)
         fila.scrollLeft -= fila.offsetWidth;
     });
 
-let url_pelis_latest = 'https://api.themoviedb.org/3/movie/upcoming?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US&page=1'
+    let url_pelis_latest = 'https://api.themoviedb.org/3/movie/upcoming?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US&page=1'
 
     fetch (url_pelis_latest)
         .then(function(response){
@@ -50,21 +47,38 @@ let url_pelis_latest = 'https://api.themoviedb.org/3/movie/upcoming?api_key=0317
         .then(function(data){
             console.log(data)
             let info = data.results
-            let conteiner = document.querySelector('.pelis_lat');
-            let peliculas_latest = '';
+            let conteiner = document.querySelector('.carrusel2');
+            let url_img = 'https://image.tmdb.org/t/p/w100/'
+            let peliculas_recientes = '';
+
             for(let i = 0; i<info.length; i++){
-                peliculas_latest += `<article>
+                peliculas_recientes += `<article class="pelis">
                                             <a href='detalle_pelicula.html?id=${info[i].id}'>
-                                            <img src=${"https://image.tmdb.org/t/p/w200/" + info[i].poster_path} alt=''/>                                            
-                                            <p>${info[i].original_title}</p>
+                                                <img src=${"https://image.tmdb.org/t/p/w300/" + info[i].poster_path} alt='' />
+                                                <p>${info[i].title}</p>
                                             </a>
                                         </article>`
             }
-            conteiner.innerHTML = peliculas_latest;
+            conteiner.innerHTML = peliculas_recientes;
         })
         .catch(function(error){
             console.log(error);
         })
+
+
+    let fila2 = document.querySelector('.contenedor_carrusel2');        
+    let flechaIzquierda2 = document.getElementById('flecha_izquierda2');
+    let flechaDerecha2 = document.getElementById('flecha_derecha2');
+        
+    //  ----- ----- Event Listener para la flecha derecha. ----- -----
+    flechaDerecha2.addEventListener('click', () => {
+        fila2.scrollLeft += fila2.offsetWidth;
+    });
+        
+    //  ----- ----- Event Listener para la flecha izquierda. ----- -----
+    flechaIzquierda2.addEventListener('click', () => {
+        fila2.scrollLeft -= fila2.offsetWidth;
+    });
 
 
 let url_series = 'https://api.themoviedb.org/3/tv/on_the_air?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US&page=1'
@@ -76,12 +90,12 @@ fetch (url_series)
     .then(function(data){
         console.log(data)
         let info = data.results
-        let conteiner = document.querySelector('.ser');
+        let conteiner = document.querySelector('.carrusel3');
         let series = '';
         for(let i = 0; i<info.length; i++){
-            series += `<article>
+            series += `<article class="pelis">
                                     <a href='detalle_pelicula.html?id=${info[i].id}'>
-                                        <img src=${"https://image.tmdb.org/t/p/w200/" + info[i].poster_path} alt='' />                                        
+                                        <img src=${"https://image.tmdb.org/t/p/w300/" + info[i].poster_path} alt='' />                                        
                                         <p>${info[i].original_name}</p>
                                     </a>
                                     </article>`
@@ -91,6 +105,20 @@ fetch (url_series)
     .catch(function(error){
         console.log(error);
     })
+
+    let fila3 = document.querySelector('.contenedor_carrusel3');
+    let flechaIzquierda3 = document.getElementById('flecha_izquierda3');
+    let flechaDerecha3 = document.getElementById('flecha_derecha3');
+    
+    // ? ----- ----- Event Listener para la flecha derecha. ----- -----
+    flechaDerecha3.addEventListener('click', () => {
+        fila3.scrollLeft += fila3.offsetWidth;
+    });
+    
+    // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+    flechaIzquierda3.addEventListener('click', () => {
+        fila3.scrollLeft -= fila3.offsetWidth;
+    });
 
     //Capturo el form de busqueda
     let form = document.querySelector('form');
